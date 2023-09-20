f4234429ccc184ad1c426936a45170baa08c46ca
Federico <federicohernanerceg@gmail.com>
2023-09-20 16:55:27 -0300

    mod

--- src/Components/DataMainProducts.js
+++ src/Components/DataMainProducts.js
@@ -25,7 +25,7 @@ const DataMainProducts = () => {
         {products.map((p) => {
             return(
                 <div key={p.id}>
-                    <MainProducts img={p.imagen} nombre={p.nombre} descripcion={p.descripcion} precio={p.precio} />
+                    <MainProducts imagen={p.imagen} nombre={p.nombre} descripcion={p.descripcion} precio={p.precio} />
                 </div>
             )
         })}
