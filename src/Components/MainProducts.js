ad102d0bbb470e562e0e6bff67c9355f4960ff5c
Federico <federicohernanerceg@gmail.com>
2023-09-21 16:28:38 -0300

    mod

--- src/Components/MainProducts.js
+++ src/Components/MainProducts.js
@@ -1,8 +1,7 @@
 import { useState } from "react";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const MainProducts = ({ imagen, nombre, descripcion, precio }) => {
-    
   const [contador, setContador] = useState(0);
   const sumar = () => {
     setContador(contador + 1);
@@ -22,21 +21,15 @@ const MainProducts = ({ imagen, nombre, descripcion, precio }) => {
           <p>{descripcion}</p>
           <h5>Precio: ${precio}</h5>
           <div className="row my-3 g-2">
-            <Link to='/' className="col">
-              <button className="btn btn-primary" onClick={sumar}>
-                Agregar al carrito
-              </button>
-            </Link>
-            <Link to='/' className="col">
-              <button className="btn btn-primary" onClick={restar}>
-                Quitar del carrito
-              </button>
-            </Link>
-            <Link to='/' className="col">
-              <button className="btn btn-primary" onClick={eliminar}>
-                Eliminar del carrito
-              </button>
-            </Link>
+            <button className="btn btn-primary" onClick={sumar}>
+              Agregar al carrito
+            </button>
+            <button className="btn btn-primary" onClick={restar}>
+              Quitar del carrito
+            </button>
+            <button className="btn btn-primary" onClick={eliminar}>
+              Eliminar del carrito
+            </button>
           </div>
           <h5>agregador al carrito: {contador}</h5>
         </div>
